16828e7114f94ddd532fcc26cdd4506cb90bb911
ashik081 <freelancerashik81@gmail.com>
2026-06-18 08:55:14 +0600

    added enviromental setup

---
+++ src/lib/auth.js
@@ -0,0 +1,34 @@
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("zenithFit");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        defaultValue: "user",
+      },
+    },
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 60 * 24 * 60,
+    },
+  },
+  plugins: [jwt()],
+});
